d0061f107e1072bb18425ce6d72731e3efe687f2
Sean <seanzhao2007@gmail.com>
2024-01-13 12:55:16 -0500

    changed dbConn

--- client/src/components/Editor.js
+++ client/src/components/Editor.js
@@ -3,6 +3,8 @@ import axios from "axios";
 import Markdown from "react-markdown";
 import useAuth from "../hooks/useAuth";
 
+const LOGIN_URL = "/api/problems";
+
 function Editor() {
   const { auth } = useAuth();
   const [username, setUsername] = useState("");
@@ -16,6 +18,10 @@ function Editor() {
   const [body, setBody] = useState("");
   const [inputspec, setInputspec] = useState("");
   const [outputspec, setOutputspec] = useState("");
+  const [sampleinput, setSamplenput] = useState("");
+  const [sampleoutput, setSampleoutput] = useState("");
+  const [input, setInput] = useState("");
+  const [output, setOutput] = useState("");
 
   const updateTitle = (e) => {
     const value = e.target.value;
@@ -33,11 +39,50 @@ function Editor() {
     const value = e.target.value;
     setOutputspec(value);
   };
-
-  const postBlog = async () => {
-    await axios.post("http://localhost:8080/api/problems/", {});
+  const updateSampleinput = (e) => {
+    const value = e.target.value;
+    setSamplenput(value);
+  };
+  const updateSampleoutput = (e) => {
+    const value = e.target.value;
+    setSampleoutput(value);
+  };
+  const updateInput = (e) => {
+    const value = e.target.value;
+    setInput(value);
+  };
+  const updateOutput = (e) => {
+    const value = e.target.value;
+    setOutput(value);
   };
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post(
+        LOGIN_URL,
+        JSON.stringify({ user, pwd }),
+        {
+          headers: { "Content-Type": "application/json" },
+          withCredentials: true,
+        }
+      );
+      console.log(JSON.stringify(response?.data));
+      const accessToken = response?.data?.accessToken;
+      const rating = response.data.rating;
+    } catch (err) {
+      if (!err?.response) {
+        setErrMsg("No Server Response");
+      } else if (err.response?.status === 400) {
+        setErrMsg("Missing Username or Password");
+      } else if (err.response?.status === 401) {
+        setErrMsg("Unauthorized");
+      } else {
+        setErrMsg("Login Failed");
+      }
+    }
+  };
   return (
     <>
       <div className="flex justify-around">
@@ -74,8 +119,40 @@ function Editor() {
             value={outputspec}
             onChange={updateOutputspec}
           />
+          <h1 className="text-white text-5xl font-medium mb-8 select-none">
+            Sample Input
+          </h1>
+          <textarea
+            className="mb-12 w-full bg-neutral-700 resize-none rounded-3xl px-6 py-4 outline-none h-40 focus:outline-teal-400"
+            value={sampleinput}
+            onChange={updateSampleinput}
+          />
+          <h1 className="text-white text-5xl font-medium mb-8 select-none">
+            Sample Output
+          </h1>
+          <textarea
+            className="mb-12 w-full bg-neutral-700 resize-none rounded-3xl px-6 py-4 outline-none h-40 focus:outline-teal-400"
+            value={sampleoutput}
+            onChange={updateSampleoutput}
+          />
+          <h1 className="text-white text-5xl font-medium mb-8 select-none">
+            Input
+          </h1>
+          <textarea
+            className="mb-12 w-full bg-neutral-700 resize-none rounded-3xl px-6 py-4 outline-none h-40 focus:outline-teal-400"
+            value={input}
+            onChange={updateInput}
+          />
+          <h1 className="text-white text-5xl font-medium mb-8 select-none">
+            Output
+          </h1>
+          <textarea
+            className="mb-12 w-full bg-neutral-700 resize-none rounded-3xl px-6 py-4 outline-none h-40 focus:outline-teal-400"
+            value={output}
+            onChange={updateOutput}
+          />
           <button className="text-black bg-teal-400 px-8 py-4 text-medium rounded-full">
-            Post!
+            Create!
           </button>
         </form>
         <div className="my-8 w-2/5">
@@ -90,13 +167,25 @@ function Editor() {
             Input Specification
           </h1>
           <Markdown className="w-full prose prose-invert">{inputspec}</Markdown>
-          <hr class="h-1 mx-auto my-8 bg-neutral-700 border-0 rounded" />
           <h1 className="text-white text-2xl font-medium my-8 select-none">
             Output Specification
           </h1>
           <Markdown className="w-full prose prose-invert">
             {outputspec}
           </Markdown>
+          <hr class="h-1 mx-auto my-8 bg-neutral-700 border-0 rounded" />
+          <h1 className="text-white text-2xl font-medium my-8 select-none">
+            Sample Input
+          </h1>
+          <p className="mb-12 w-full bg-neutral-700 resize-none rounded-3xl px-6 py-4 outline-none h-full">
+            {sampleinput}
+          </p>
+          <h1 className="text-white text-2xl font-medium my-8 select-none">
+            Sample Output
+          </h1>
+          <p className="mb-12 w-full bg-neutral-700 resize-none rounded-3xl px-6 py-4 outline-none h-full">
+            {sampleoutput}
+          </p>
         </div>
       </div>
     </>
--- server/server.js
+++ server/server.js
@@ -1,16 +1,16 @@
-require('dotenv').config();
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const http = require("http");
 const cors = require("cors");
-const corsOptions = require('./config/corsOptions');
+const corsOptions = require("./config/corsOptions");
 const { Server } = require("socket.io");
-const verifyJWT = require('./middleware/verifyJWT');
-const cookieParser = require('cookie-parser');
-const credentials = require('./middleware/credentials')
-const mongoose = require('mongoose');
-const connectDB = require('./config/dbConn');
-const socketController = require('./controllers/socketController');
+const verifyJWT = require("./middleware/verifyJWT");
+const cookieParser = require("cookie-parser");
+const credentials = require("./middleware/credentials");
+const mongoose = require("mongoose");
+const connectDB = require("./config/dbConn");
+const socketController = require("./controllers/socketController");
 
 //Initialize Socket.io
 const server = http.createServer(app);
@@ -36,17 +36,17 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cookieParser());
 
-app.use('/register', require('./routes/register'));
-app.use('/auth', require('./routes/auth'));
-app.use('/refresh', require('./routes/refresh'));
-app.use('/logout', require('./routes/logout'));
+app.use("/register", require("./routes/register"));
+app.use("/auth", require("./routes/auth"));
+app.use("/refresh", require("./routes/refresh"));
+app.use("/logout", require("./routes/logout"));
 
 app.use(verifyJWT);
-app.use('/problems', require('./routes/api/problems'));
-app.use('/users', require('./routes/api/users'));
+app.use("/api/problems", require("./routes/api/problems"));
+app.use("/api/users", require("./routes/api/users"));
 
-mongoose.connection.once('open', () => {
-  console.log('Connected to MongoDB');
+mongoose.connection.once("open", () => {
+  console.log("Connected to MongoDB");
   server.listen(8080, () => {
     console.log("SERVER RUNNING");
   });
